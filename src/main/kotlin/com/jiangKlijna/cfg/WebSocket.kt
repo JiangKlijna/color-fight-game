8803fba3b66c782a57434ef902930b779a3b52d7
jiangKlijna <leil729564090@outlook.com>
2018-11-07 23:14:31 +0800

    :fire:add WebSocketClient

--- src/main/kotlin/com/jiangKlijna/cfg/WebSocket.kt
+++ src/main/kotlin/com/jiangKlijna/cfg/WebSocket.kt
@@ -1,10 +1,28 @@
 package com.jiangklijna.cfg
 
 import io.vertx.core.http.ServerWebSocket
+import io.vertx.core.http.WebSocketFrame
+import java.util.concurrent.ConcurrentHashMap
 
-object WebSocket {
+class WebSocket(val id: String, val socket: ServerWebSocket) {
 
-    val handler = { socket: ServerWebSocket ->
+    init {
+        socket.frameHandler(this::frameHandler)
+        socket.closeHandler { clients.remove(id) }
+    }
 
+    private fun frameHandler(frame: WebSocketFrame) {
+    }
+
+    companion object {
+
+        private val clients = ConcurrentHashMap<String, WebSocket>()
+
+        val handler = { socket: ServerWebSocket ->
+            val id = socket.binaryHandlerID()
+            if (id !in clients) {
+                clients[id] = WebSocket(id, socket)
+            }
+        }
     }
 }
