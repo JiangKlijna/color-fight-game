11c985fca33de90af2e60a199bc489db5cb7f324
jiangKlijna <leil729564090@outlook.com>
2018-11-10 20:27:03 +0800

    :fire:update createroom

--- src/main/kotlin/com/jiangKlijna/cfg/GameRoom.kt
+++ src/main/kotlin/com/jiangKlijna/cfg/GameRoom.kt
@@ -1,19 +1,32 @@
 package com.jiangklijna.cfg
 
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentSkipListMap
+import java.util.concurrent.CopyOnWriteArrayList
 
-class GameRoom(val title: String, val size: Int) {
-    private val players = Array<GamePlayer?>(size) { null }
+class GameRoom(val title: String, val length: Int) {
+    private val players = CopyOnWriteArrayList<GamePlayer?>()
+
+    fun join(player: GamePlayer) = if (players.size == length) false else players.add(player)
+
+    fun size() = players.size
+
+    fun id() = hashCode()
+
+    operator fun contains(player: GamePlayer) = players.contains(player)
+
+    operator fun get(i: Int) = players[i]
 
     companion object {
-        private val rooms = ConcurrentHashMap<String, GameRoom>()
+        private val rooms = ConcurrentSkipListMap<Int, GameRoom>()
 
-        operator fun get(id: String) = rooms[id]
+        operator fun get(id: Int) = rooms[id]
 
-        operator fun contains(id: String) = rooms.contains(id)
+        operator fun contains(id: Int) = rooms.contains(id)
 
         fun createRoom(player: GamePlayer, title: String, size: Int) {
-            
+            val room = GameRoom(title, size)
+            rooms[room.id()] = room
+            room.join(player)
         }
     }
 }
