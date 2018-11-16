c9316be7aa85b7ecea8c0a37543520141ce7804d
jiangKlijna <leil729564090@outlook.com>
2018-11-16 17:29:34 +0800

    :fire:init lib.js

--- src/main/resources/html/js/lib.js
+++ src/main/resources/html/js/lib.js
@@ -1,4 +1,11 @@
 // simple jquery
 (function () {
-
+    let El = function (dom) {
+        this.dom = dom;
+    };
+    El.of = function () {
+        if (arguments.length === 0) return new El(null);
+        else if (arguments.length === 1) return new El(arguments[0]);
+        else return new El(Array.from(arguments));
+    };
 })();
