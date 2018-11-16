ea3c67cf42a5ad7a1ddd269343a42a9e13d50ad8
jiangKlijna <leil729564090@outlook.com>
2018-11-16 17:32:53 +0800

    :fire:update lib.js

--- src/main/resources/html/js/lib.js
+++ src/main/resources/html/js/lib.js
@@ -1,11 +1,14 @@
 // simple jquery
-(function () {
+var Lib = (function () {
+    let self = {};
     let El = function (dom) {
         this.dom = dom;
+        this.isArray = dom instanceof Array
     };
-    El.of = function () {
+    self.of = function () {
         if (arguments.length === 0) return new El(null);
         else if (arguments.length === 1) return new El(arguments[0]);
         else return new El(Array.from(arguments));
     };
+    return self;
 })();
