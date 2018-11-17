d682dbfecdcc6f283e6724137828dcdfb601bce0
jiangKlijna <leil729564090@outlook.com>
2018-11-17 10:45:25 +0800

    :fire:add El.of fun

--- src/main/resources/html/js/lib.js
+++ src/main/resources/html/js/lib.js
@@ -3,28 +3,23 @@ var Lib = (function () {
     var El = function (dom) {
         this.dom = dom;
     };
+    El.of = function (doms) {
+        var arr = [];
+        for (var i = 0; i < doms.length; i++) {
+            arr.push(new El(doms[i]));
+        }
+        return arr;
+    };
     El.prototype.kid = function () {
         var doms = this.dom.children;
-        if (doms.length === 0) return doms;
-        else if (doms.length === 1) return new El(doms[0]);
-        else {
-            var arr = [];
-            for (var i = 0; i < doms.length; i++) {
-                arr.push(new El(doms[i]));
-            }
-            return arr;
-        }
+        return El.of(doms);
     };
     El.prototype.one = function (selector) {
         return new El(this.dom.querySelector(selector));
     };
     El.prototype.all = function (selector) {
-        var arr = [];
         var doms = document.querySelectorAll(selector);
-        for (var i = 0; i < doms.length; i++) {
-            arr.push(new El(doms[i]));
-        }
-        return arr;
+        return El.of(doms);
     };
     return new El(document);
 })();
