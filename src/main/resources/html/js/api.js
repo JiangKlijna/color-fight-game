891a402ced71cb0b1d73278e093baabf93fba4e9
jiangKlijna <leil729564090@outlook.com>
2018-11-22 22:11:18 +0800

    :fire:init api

--- src/main/resources/html/js/api.js
+++ src/main/resources/html/js/api.js
@@ -1,7 +1,22 @@
 // api for game server
 var api = (function () {
     var self = {};
-
-
+    var skt = null;
+    self.init = function () {
+        skt = new WebSocket("ws://localhost:8080/");
+        skt.onopen = self.onOpen;
+        skt.onclose = self.onClose;
+        skt.onmessage = self.onMessage;
+    };
+    self.onOpen = function (e) {
+        console.log("websocket onopen", e);
+    };
+    self.onClose = function (e) {
+        console.log("websocket onclose", e);
+    };
+    self.onMessage = function (e) {
+        console.log("websocket onMessage", e);
+    };
+    self.init();
     return self;
 })();
